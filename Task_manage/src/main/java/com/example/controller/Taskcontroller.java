9daab11be63a1d16669fde67c3a46f0068bbf1e0
Yuva-SMI <48523338+Yuva-SMI@users.noreply.github.com>
2019-03-20 13:20:07 +0530

    ok

---
+++ Task_manage/src/main/java/com/example/controller/Taskcontroller.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Taskcontroller {
+	 @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+		 public ModelAndView login(){
+		        ModelAndView modelAndView = new ModelAndView();
+		        modelAndView.setViewName("login.html");
+		        return modelAndView;
+		    }
+
+}

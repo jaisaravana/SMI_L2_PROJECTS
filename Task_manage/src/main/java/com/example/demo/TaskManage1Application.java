042f80a9a17a7eda57fc44337bad55205d6fe251
Yuva-SMI <48523338+Yuva-SMI@users.noreply.github.com>
2019-03-21 12:13:55 +0530

    okay

---
+++ Task_manage/src/main/java/com/example/demo/TaskManage1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManage1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManage1Application.class, args);
+	}
+
+}
